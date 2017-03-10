08d3fc0ef8c87bcfefec4f8399bcbb7c977a7861
Jo-Anne Tay <tayj@gb-lt-mj34316.local>
2017-03-10 13:50:48 +0000

    Modify authentication scheme

--- amigos/settings.py
+++ amigos/settings.py
@@ -148,8 +148,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.BasicAuthentication',
+       # 'rest_framework.authentication.BasicAuthentication',
        'rest_framework.authentication.TokenAuthentication',
+       'rest_framework.authentication.SessionAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
